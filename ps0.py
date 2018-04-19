d5082237608f0c383fd935d7eb16c7581b6ae8a9
Luke Young <lukeyoungdata@gmail.com>
2018-04-19 15:16:33 -0400

    Created initial file with 2 declared variables
    name for holding the string name
    date for holding the string date of birth

---
+++ ps0.py
@@ -0,0 +1,17 @@
+# -*- coding: cp1252 -*-
+# Luke Young
+# 2018 04 19
+# MIT OC - CS600 - Introduction to Computer Science and Programming
+# Problem Set 0: Itroduction to Python and IDLE
+# Program: Entering and Printing your name
+# 
+# Write a program that does the following in order:
+# Asks the user to enter his/her date of birth.
+# Asks the user to enter his/her last name.
+# Prints out the user’s last name and date of birth, in that order. 
+
+birth = ""
+name = ""
+
+
+birth = raw_input("Hello, enter your date of birth: ")
