1fa068f03f4e0317774f91f0634e0e0000c551f8
Luke Young <lukeyoungdata@gmail.com>
2018-04-19 15:28:26 -0400

    Finished the file
    Now takes in date of birth and name and then prints name + date with a space in between.
    
    Also fixed the special character in "user's"

--- ps0.py
+++ ps0.py
@@ -1,17 +1,23 @@
 # -*- coding: cp1252 -*-
-# Luke Young
-# 2018 04 19
 # MIT OC - CS600 - Introduction to Computer Science and Programming
 # Problem Set 0: Itroduction to Python and IDLE
+# Name: Luke Young
+# Collaborators: None
+# Time Spent: 00:12 (hr:min)
+# 2018 04 19
+
 # Program: Entering and Printing your name
 # 
 # Write a program that does the following in order:
 # Asks the user to enter his/her date of birth.
 # Asks the user to enter his/her last name.
-# Prints out the user’s last name and date of birth, in that order. 
+# Prints out the user's last name and date of birth, in that order. 
 
 birth = ""
 name = ""
 
 
 birth = raw_input("Hello, enter your date of birth: ")
+name  = raw_input("Now, what is your surname? ")
+
+print(name + " " + birth)
